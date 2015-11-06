64e803425954c4027fccfe39340c2bc47be1d1d1
Browomir <tomczak.sebastian.st@gmail.com>
2015-11-06 18:31:30 +0100

    class add

---
+++ src/Encryption.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OpenEncryption;
+
+class Encryption
+{
+    const ENCRYPT_METHOD = 'AES-128-ECB';
+    const SECRET_KEY_LENGTH = 16;
+
+    /**
+     * encrypt given string with ENCRYPT_METHOD e.g. AES-128-ECB
+     * @param $input
+     * @param $salt
+     * @param $username
+     * @return string base64 encoded
+     */
+    public static function encrypt($input, $salt, $username)
+    {
+        $secretKey = self::getKey($salt, $username);
+        $encrypted = openssl_encrypt($input, self::ENCRYPT_METHOD, $secretKey, OPENSSL_RAW_DATA);
+        $encrypted = base64_encode($encrypted);
+
+        return $encrypted;
+    }
+
+    /**
+     * decrypt given hash string with ENCRYPT_METHOD e.g. AES-128-ECB
+     * @param $hash
+     * @param $salt
+     * @param $username
+     * @return string
+     */
+    public static function decrypt($hash, $salt, $username)
+    {
+        $secretKey = self::getKey($salt, $username);
+        $decrypted = openssl_decrypt(base64_decode($hash), self::ENCRYPT_METHOD, $secretKey, OPENSSL_RAW_DATA);
+
+        return $decrypted;
+    }
+
+    /**
+     * generate key to encryption/decryption
+     * @param $salt
+     * @param $username
+     * @return string
+     */
+    private static function getKey($salt, $username)
+    {
+        $secretString = $salt . $username;
+        $secretKey = sha1($secretString);
+        $secretKey = substr($secretKey, 0, self::SECRET_KEY_LENGTH);
+
+        return $secretKey;
+    }
+}
